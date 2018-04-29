1471d5f826b3caca153648b73e7543d29c48123b
Farooq Azam Khan <farooq1.khan@ryerson.ca>
2018-04-29 14:50:03 -0400

    added array of pipes and indication of bird hitting pipe

--- pipe.js
+++ pipe.js
@@ -9,10 +9,16 @@ class Pipe
     this.x = width;
     this.w = 20; // width of all rectangles
     this.speed = 3; // speed of how fast the pipe is moving
+
+    this.highlight = false;
   }
   show()
   {
     fill(255);
+    if (this.highlight)
+    {
+      fill(255, 0, 0, 100);
+    }
     // top rectangle
     rect(this.x,  0, this.w, this.top);
     // bottom rectangel
@@ -22,5 +28,25 @@ class Pipe
   update()
   {
     this.x -= this.speed; // -= because moving to the left
+  }
+
+  offscreen()
+  {
+    // if width is less than zero
+    return this.x < -this.w;
+  }
+
+  hits(bird)
+  {
+    if(bird.y < this.top || bird.y > height - this.bottom)
+    {
+      if (bird.x > this.x && bird.x < this.x + this.w)
+      {
+        this.highlight = true;
+        return true;
+      }
+    }
+    this.highlight = false; 
+    return false;
   }
 }
--- sketch.js
+++ sketch.js
@@ -1,28 +1,43 @@
-let b; // bird object
+let bird; // bird object
 let pipes = []; // array of pipes
 function setup()
 {
   createCanvas(400, 600);
-  b = new Bird();
+  bird = new Bird();
   pipes.push(new Pipe());
 }
 
 function draw()
 {
   background(51);
-  b.update();
-  b.show();
 
-  // every fourty frames add new pipe
-  if (frameCount % 40 == 0)
-  {
-    pipes.push(new Pipe());
-  }
 
-  for (var i = 0; i< pipes.length; i++)
+
+  for (var i = pipes.length-1; i>=0; i--)
   {
     pipes[i].show();
     pipes[i].update();
+
+    if(pipes[i].hits(bird))
+    {
+      console.log("pipe hit bird");
+    }
+
+    // remove pipe
+    if (pipes[i].offscreen())
+    {
+      pipes.splice(i, 1);
+    }
+  }
+
+  bird.update();
+  bird.show();
+
+  // every 100 frames add new pipe
+  if (frameCount % 100 == 0)
+  {
+    pipes.push(new Pipe());
+    console.log("pipes length: " + pipes.length);
   }
 
 }
@@ -32,7 +47,7 @@ function keyPressed()
 {
   if (key = ' ')
   {
-    b.up();
+    bird.up();
     console.log("space");
   }
 }
